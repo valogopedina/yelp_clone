cc7dca4b4a98fe197d0fb20c26d7de8d6c8c7be8
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-03-01 17:05:20 +0000

    Added destroy method to delete restaurants and flash notice for confirmation

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -29,4 +29,12 @@ class RestaurantsController < ApplicationController
     @restaurant.update(restaurant_params)
     redirect_to '/restaurants'
   end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:notice] = 'Restaurant deleted successfully'
+    redirect_to '/restaurants'
+  end
+
 end
