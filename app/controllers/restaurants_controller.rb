a16bebb13d13fb5c17014c0780368ced99245c18
David Parry <drjparry@gmail.com>
2016-03-02 11:13:22 +0100

    added edit function

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,5 +4,30 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
   end
+
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name)
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
 end
--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-feature 'restaurants' do 
-	context 'no restaurants have been added' do 
-		scenario 'should display a prompt to add a restaurant' do 
+feature 'restaurants' do
+	context 'no restaurants have been added' do
+		scenario 'should display a prompt to add a restaurant' do
 			visit '/restaurants'
 			expect(page).to have_content 'No restaurants yet'
 			expect(page).to have_link 'Add a restaurant'
@@ -20,7 +20,7 @@ feature 'restaurants' do
 		end
 	end
 
-	context 'creating restaurants' do 
+	context 'creating restaurants' do
 		scenario 'prompts user to fill out a form, then displays the new restaurant' do
 		visit '/restaurants'
 		click_link 'Add a restaurant'
@@ -30,4 +30,26 @@ feature 'restaurants' do
 		expect(current_path).to eq '/restaurants'
 		end
 	end
+
+	context 'viewing restaurants' do
+		let!(:kfc){Restaurant.create(name:'KFC')}
+		scenario 'lets a user view a restaurant' do
+			visit '/restaurants'
+			click_link 'KFC'
+			expect(page).to have_content 'KFC'
+			expect(current_path).to eq "/restaurants/#{kfc.id}"
+		end
+	end
+
+	context 'updating restaurants' do
+		let!(:kfc){Restaurant.create(name:'KFC')}
+		scenario 'user can edit existing restaurant' do
+			visit '/restaurants'
+			click_link 'Edit'
+			fill_in 'Name', with: 'Maccas'
+			click_button 'Update Restaurant'
+			expect(page).to have_content 'Maccas'
+		end
+	end
+
 end
