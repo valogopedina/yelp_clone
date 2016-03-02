8518ce07d190672ed632d933a9c5c8dd6eb26e37
valogopedina <valentina.monetta@tiscali.it>
2016-03-02 09:32:26 +0000

    WIP Added feature test for adding reviews, nested routes and added controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
 
   resources :restaurants
 
+  resources :restaurants do
+    resources :reviews
+  end
+
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
